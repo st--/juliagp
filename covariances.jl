6ae2da68e007e702cf432c2635bfe71736def2fd
ST John <stj26@cam.ac.uk>
2015-04-22 15:40:27 +0100

    Add covariance derivatives w.r.t. input values for CovSEiso

--- covariances.jl
+++ covariances.jl
@@ -22,7 +22,21 @@ length(k::CovSEiso) = 1
 cov{T}(k::CovSEiso, x1::T, x2::T) = exp(-0.5vecnorm(x1 - x2)^2 / k.ell^2)
 
 covderiv{T}(k::CovSEiso, i::Integer, x1::T, x2::T) = vecnorm(x1 - x2)^2 / k.ell^3 * cov(k, x1, x2)
+# derivative w.r.t. i'th hyperparameter of covariance kernel
 
+function cov_d{T}(k::CovSEiso, x1::T, x2::T)
+# derivative w.r.t. x2
+    (x1 - x2)/k.ell^2 * cov(k, x1, x2)
+end
+
+covd_(k::CovSEiso, x1, x2) = - cov_d(k, x1, x2) # derivative w.r.t. x1
+
+function covdd{T}(k::CovSEiso, x1::T, x2::T) 
+# second (mixed) derivative w.r.t x1 and x2
+    cov(k, x1, x2) / k.ell^2 * (1 - (x1 - x2)^2/k.ell^2)
+end
+
+covd2(k::CovSEiso, x1, x2) = - covdd(k, x1, x2) # second derivative w.r.t. x1 or x2
 
 immutable CovSEard{N<:Number} <: VectorKernel
     ells::Vector{N}
